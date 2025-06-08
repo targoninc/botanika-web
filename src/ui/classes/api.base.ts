fb1962685fb98b9538f2cff9df375e95f4e92038
alexa <35202909+loudar@users.noreply.github.com>
2025-06-08 15:56:24 +0200

    check for hostname to determine api url

--- src/ui/classes/api.base.ts
+++ src/ui/classes/api.base.ts
@@ -2,7 +2,7 @@ import {toast} from "./ui";
 import {ToastType} from "../enums/ToastType";
 
 export class ApiBase {
-    static baseUrl = `http://localhost:48678`;
+    static baseUrl = window.location.hostname === "localhost" ? `http://localhost:48678` : "https://botanika-api.targoninc.com";
 
     static async stream(url: string, data = {}, sendCredentials = false) {
         const res = await fetch(ApiBase.baseUrl + url, {
