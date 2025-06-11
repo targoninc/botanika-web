f8ae4225795f8a75b8aa38bceef0a32470585525
MarioCakeDev <github@mariocake.de>
2025-06-11 02:39:22 +0200

    Revert shortcut changes.

--- src/models/shortcuts/ShortcutConfiguration.ts
+++ src/models/shortcuts/ShortcutConfiguration.ts
@@ -1,9 +1,3 @@
 import {Shortcut} from "./Shortcut";
 
-export type ShortcutConfiguration = Record<Shortcut, ShortcutConfig>;
-export type ShortcutConfig = {
-    key: string;
-    ctrl: boolean;
-    alt: boolean;
-    shift: boolean;
-}
+export type ShortcutConfiguration = Record<Shortcut, string>;
